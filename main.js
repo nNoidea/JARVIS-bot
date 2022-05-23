e99aceed5f59fa5f3c5273f68ade5916ca9db0ab
MilanR312 <92678773+MilanR312@users.noreply.github.com>
2022-05-23 13:10:22 +0200

    bug fixes

--- main.js
+++ main.js
@@ -1,42 +1,60 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10');
 const Discord = require('discord.js');
 const fs = require('fs');
 
+
 const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'] });
 
-const prefix = '!';
 
+const dotenv = require('dotenv');
+dotenv.config();
+const TOKEN = process.env['TOKEN'];
+const CLIENT_ID = process.env['BOT_ID']
+const TEST_GUILD_ID = process.env['TEST_GUILD_ID'];
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+
+const commands = [];
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./tinygif').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./other_commands').filter(file => file.endsWith('.js'));
+
 
 for (const file of commandFiles) {
-  const command = require(`./tinygif/${file}`);
-  client.commands.set(command.name, command);
+    const command = require(`./other_commands/${file}`);
+    commands.push(command.data.toJSON());
+    client.commands.set(command.data.name, command);
 }
 
 
-client.once('ready', () => {
-    client.user.setPresence({
-        status: 'online',
-        activity: {
-            name: '!help',
-            type: 'PLAYING'
-        }
-    });
-});
 
-
-client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
+(async () => {
     try {
-        client.commands.get(command).execute(message, args);
+        console.log("test");
+        await rest.put(
+            Routes.applicationGuildCommands(CLIENT_ID, TEST_GUILD_ID),
+            { body: commands },
+        );
+        console.log("test2");
     } catch (error) {
         console.error(error);
-        message.reply('there was an error trying to execute that command!');
     }
+})();
+
+
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
-client.login("OTY4MjMyNjIyNDAyNzExNTgy.GT4BQo.ULT2G09YBqagACvi66mPFt7AkwS2kIXGCRJQz0")
+client.login(TOKEN)
