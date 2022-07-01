4d2f5ff0dcd49e27767f249a29cf36c5d3d767f6
nNoidea <58337790+nNoidea@users.noreply.github.com>
2022-07-02 00:06:02 +0200

    - unarchive function - vanilla message function - bunch of blacklist booleans to ignore if needed - mentions the creator of files with or without a notification

--- index.ts
+++ index.ts
@@ -1,54 +1,63 @@
-import { Message, TextChannel } from "discord.js"
+import { Collection, Guild, Message, TextChannel, User } from "discord.js"
 
 const Discord = require("discord.js")
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 require("dotenv").config()
 
-client.login(process.env.TOKEN) // Log into discord
-const currentYear = new Date().getFullYear(); // Gets the current year
-let blacklist_categories = process.env.BLACK_LISTED_CATEGORIES?.split(",") // Gets the blacklisted categories where the bot shouldn't work
+client.login(process.env.TOKEN) // Log into discord.
+const currentYear = new Date().getFullYear(); // Gets the current year.
+
+let blacklisted_servers = process.env.BLACKLISTED_SERVERS?.split(",") // Gets the blacklisted server list (mostly to ignore test servers etc).
+let blacklisted_categories = process.env.BLACKLISTED_CATEGORIES?.split(",") // Gets the blacklisted categories where the bot shouldn't work.
+let blacklisted_channels = process.env.BLACKLISTED_CHANNELS?.split(",")// Gets the blacklisted channels where the bot shouldn't work.
+let blacklisted_users = process.env.BLACKLISTED_USERS?.split(",") // Gets the blacklisted user list.
+let bot_id = process.env.BOT_ID // Bot's own id, to ignore his own messages if needed.
 
 
-client.on("ready", ALIVE) // Logs when the bot is ready
+client.on("ready", ALIVE) // Logs when the bot is ready.
 function ALIVE() {
     console.log("BOT IS ACTIVE")
 }
 
-client.on("messageCreate", message_handler) // When a message send by someone, sends the message to `message_handler`
+client.on("messageCreate", message_handler) // When a message send by someone, sends the message to `message_handler`.
 function message_handler(message: Message) {
-    if (blacklist(message)) {
-        archive_pdf_attachments(message)
-        // ALL THE OTHER FUNCTIONS COME HERE!!!
+    if (blacklist_server(message)) {
+        if (blacklist_category(message)) {
+            archive_pdf_attachments(message)
+            // ALL THE OTHER FUNCTIONS COME HERE!!!
+        }
     }
 }
 
 
 async function archive_pdf_attachments(message: Message) {
     let channel = message.channel
-    if (!(channel instanceof TextChannel)) { return } // Makes sures no bad types get through
+    if (!(channel instanceof TextChannel)) { return } // Makes sure no bad types get through
 
     let amount_of_attachments = message.attachments.size // Gets the amount of files
     let array: any = []
 
     if (amount_of_attachments > 0) {
-        // filters out the pdf files and collects them in an array
+        // Filters out the pdf files and collects them in an array.
         for (let i = 0; i < amount_of_attachments; i++) {
-            let file_name = message.attachments.at(i)?.name // gets the file name
-            let file_extension = get_file_extension(file_name!) // gets the file extension
+            let file_name = message.attachments.at(i)?.name // Gets the file name.
+            let file_extension = get_file_extension(file_name!) // Gets the file extension.
 
             if (file_extension == "pdf") {
-                array.push(message.attachments.at(i)) // Saves attachment that are a pdf into an array
+                array.push(message.attachments.at(i)) // Saves attachment that are a pdf into an array.
             }
         }
-        await channel.threads.fetchArchived() // Caches the archived threads, active ones are normally automatically cached. 'await' is needed, because the bot does not wait for that function and might continue executing following code without caching.
 
-        let thread = channel.threads.cache.find((x: any) => x.name === `c - ${ currentYear }`);
-        await thread?.setArchived(false); // Waits, because unarchiving is sometimes slower than the bot
-        // If the thread does not exist, creates a thread
-        if (thread == undefined) {
-            thread = await create_thread(`c - ${ currentYear }`, channel)
+        let channel_name = channel.name
+        let thread_name = `${ channel_name } - ${ currentYear }`
+
+        let thread = await unarchive_thread(thread_name, channel)
+        if (thread == false) {
+            thread = await create_thread(thread_name, channel)
         }
 
+        let user_id = message.guild?.ownerId // Gets the id of the user
+        await thread.send(vanilla_message(`:flushed: <@${ user_id }> :partying_face:`, []))
         // Sends all the files to the thread
         for (let x = 0; x < array.length; x++) {
             await thread.send({
@@ -56,31 +65,65 @@ async function archive_pdf_attachments(message: Message) {
             })
         }
     }
-    else { return 0 }
+    else { console.log("no attachments") }
 }
 
+// Sends a vanilla (not embedded) message. You can also mention users with text interpolation (`SOMETHING <@${ user_id }> SOMETHING`). 
+// Leave the `notification_user_id_array` empty ([]) if you don't want anyone to get pinged, or fill it with user_id's to ping them.
+function vanilla_message(message_content: string, notification_user_id_array: string[]) {
+    let message: any = {
+        content: message_content,
+        allowedMentions: { users: notification_user_id_array },
+    }
+    return message
+}
 
-function blacklist(message: Message) {
-    let channel = message.channel
-    if (!(channel instanceof TextChannel)) { return } // Just making sure no funky buisness happens
+// Ignores blacklisted servers
+function blacklist_server(message: Message) {
+    let server = message.guild
+    if (!(server instanceof Guild)) { return } // Filtering out bad types.
 
     // Checks if the category the message is typed is blacklisted or not.
-    let parentId = channel.parentId
-    if (blacklist_categories?.find(element => element == parentId)) {
-        console.log("Blacklisted")
+    let server_id = server.id
+    if (blacklisted_servers?.find(element => element == server_id)) {
+        console.log(`Blacklisted server | ${ server.name }: ${ server_id }`)
         return false
     }
     else { return true }
 }
-function unarchive_thread(name: string, channel: TextChannel) {
-    return null
+
+// Ignores blacklisted categories
+function blacklist_category(message: Message) {
+    let channel = message.channel
+    if (!(channel instanceof TextChannel)) { return } // Just making sure no funky business happens
+
+    // Checks if the category the message is typed is blacklisted or not.
+    let parent_id = channel.parentId
+    if (blacklisted_categories?.find(element => element == parent_id)) {
+        console.log(`Blacklisted category | ${ channel.name } | ${ channel.parent?.name }: ${ parent_id }`)
+        return false
+    }
+    else { return true }
+}
+
+//Unarchives the wanted thread and returns it
+async function unarchive_thread(name: string, channel: TextChannel) {
+    await channel.threads.fetchArchived() // Caches the archived threads, active ones are normally automatically cached. 'await' is needed, because the bot does not wait for that function and might continue executing following code without caching.
+
+    let thread = channel.threads.cache.find((x: any) => x.name === name);
+    await thread?.setArchived(false); // Waits, because unarchiving is sometimes slower than the bot.
+    if (thread == undefined) {
+        return false
+    }
+    else { return thread }
+
 }
 
 // Creates a thread based on arguments
 function create_thread(name: string, channel: TextChannel) {
     let thread = channel.threads.create({
         name: name,
-        autoArchiveDuration: 10080 // By default we want the threads to be at max duration for visibility
+        autoArchiveDuration: 10080 // By default we want the threads to be at max duration for visibility.
     })
     return thread
 }
@@ -88,6 +131,6 @@ function create_thread(name: string, channel: TextChannel) {
 // Gets the file extension
 function get_file_extension(file_name: string) {
     let dot_index = file_name.lastIndexOf(".")
-    let file_extension = file_name.substring(dot_index + 1) // +1 is there to not include the dot char itself
+    let file_extension = file_name.substring(dot_index + 1) // +1 is there to not include the dot char itself.
     return file_extension
 }
